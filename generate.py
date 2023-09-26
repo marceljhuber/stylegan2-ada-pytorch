dd2093c362ded73a6866d184da932a9cc0ffec94
marceljhuber <marceljulianhuber@gmail.com>
2023-09-26 19:54:20 +0200

    - Optimized code for grayscale GAN-models. - Added two gradio demo files.

--- generate.py
+++ generate.py
@@ -6,7 +6,9 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-"""Generate images using pretrained network pickle."""
+"""
+Generate images using pretrained network pickle.
+"""
 
 import os
 import re
@@ -23,7 +25,9 @@ import legacy
 #----------------------------------------------------------------------------
 
 def num_range(s: str) -> List[int]:
-    '''Accept either a comma separated list of numbers 'a,b,c' or a range 'a-c' and return as a list of ints.'''
+    '''
+    Accept either a comma separated list of numbers 'a,b,c' or a range 'a-c' and return as a list of ints.
+    '''
 
     range_re = re.compile(r'^(\d+)-(\d+)$')
     m = range_re.match(s)
@@ -37,48 +41,30 @@ def num_range(s: str) -> List[int]:
 @click.command()
 @click.pass_context
 @click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
-@click.option('--seeds', type=num_range, help='List of random seeds')
+@click.option('--seeds', type=num_range, help='List of random seeds', required=True)
 @click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
 @click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
 @click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
 @click.option('--projected-w', help='Projection result file', type=str, metavar='FILE')
 @click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
+@click.option('--vector-mode', help='Generate vector files instead of images', type=bool, default=False)
+@click.option('--silent-mode', help='Disable print statements', type=bool, default=False)
 def generate_images(
     ctx: click.Context,
     network_pkl: str,
-    seeds: Optional[List[int]],
+    seeds: List[int],
     truncation_psi: float,
     noise_mode: str,
     outdir: str,
     class_idx: Optional[int],
-    projected_w: Optional[str]
+    projected_w: Optional[str],
+    vector_mode: bool,
+    silent_mode: bool,
 ):
-    """Generate images using pretrained network pickle.
+    """Generate images using pretrained network pickle."""
 
-    Examples:
-
-    \b
-    # Generate curated MetFaces images without truncation (Fig.10 left)
-    python generate.py --outdir=out --trunc=1 --seeds=85,265,297,849 \\
-        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
-
-    \b
-    # Generate uncurated MetFaces images with truncation (Fig.12 upper left)
-    python generate.py --outdir=out --trunc=0.7 --seeds=600-605 \\
-        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
-
-    \b
-    # Generate class conditional CIFAR-10 images (Fig.17 left, Car)
-    python generate.py --outdir=out --seeds=0-35 --class=1 \\
-        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl
-
-    \b
-    # Render an image from projected W
-    python generate.py --outdir=out --projected_w=projected_w.npz \\
-        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
-    """
-
-    print('Loading networks from "%s"...' % network_pkl)
+    if not silent_mode:
+        print('Loading networks from "%s"...' % network_pkl)
     device = torch.device('cuda')
     with dnnlib.util.open_url(network_pkl) as f:
         G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
@@ -88,15 +74,17 @@ def generate_images(
     # Synthesize the result of a W projection.
     if projected_w is not None:
         if seeds is not None:
-            print ('warn: --seeds is ignored when using --projected-w')
-        print(f'Generating images from projected W "{projected_w}"')
+            print('Warning: --seeds is ignored when using --projected-w')
+
+        if not silent_mode:
+            print(f'Generating images from projected W "{projected_w}"')
         ws = np.load(projected_w)['w']
-        ws = torch.tensor(ws, device=device) # pylint: disable=not-callable
+        ws = torch.tensor(ws, device=device)
         assert ws.shape[1:] == (G.num_ws, G.w_dim)
         for idx, w in enumerate(ws):
             img = G.synthesis(w.unsqueeze(0), noise_mode=noise_mode)
             img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-            img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/proj{idx:02d}.png')
+            img = PIL.Image.fromarray(img[0].cpu().numpy()).save(f'{outdir}/proj{idx:02d}.png')
         return
 
     if seeds is None:
@@ -105,25 +93,29 @@ def generate_images(
     # Labels.
     label = torch.zeros([1, G.c_dim], device=device)
     if G.c_dim != 0:
-        if class_idx is None:
-            ctx.fail('Must specify class label with --class when using a conditional network')
         label[:, class_idx] = 1
     else:
         if class_idx is not None:
-            print ('warn: --class=lbl ignored when running on an unconditional network')
+            print('Warning: --class=lbl ignored when running on an unconditional network')
 
     # Generate images.
     for seed_idx, seed in enumerate(seeds):
-        print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
+        if not silent_mode:
+            print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
         z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+
+        if vector_mode:
+            # Convert `z` to a NumPy array and save it.
+            z_np = z.cpu().numpy()
+            np.save(os.path.join(os.getcwd(), outdir, f"{class_idx}_{seed:04d}.npy"), np.array(z_np))
+
         img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
         img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+        i = img.view(256, 256)
 
+        PIL.Image.fromarray(i.cpu().numpy()).save(f'{outdir}/{class_idx}_{seed:04d}.png')
 
 #----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    generate_images() # pylint: disable=no-value-for-parameter
-
-#----------------------------------------------------------------------------
+    generate_images()
--- projector.py
+++ projector.py
@@ -6,7 +6,9 @@
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
 
-"""Project given image to the latent space of pretrained network pickle."""
+"""
+Project given image to the latent space of pretrained network pickle.
+"""
 
 import copy
 import os
@@ -22,52 +24,55 @@ import torch.nn.functional as F
 import dnnlib
 import legacy
 
+
 def project(
-    G,
-    target: torch.Tensor, # [C,H,W] and dynamic range [0,255], W & H must match G output resolution
-    *,
-    num_steps                  = 1000,
-    w_avg_samples              = 10000,
-    initial_learning_rate      = 0.1,
-    initial_noise_factor       = 0.05,
-    lr_rampdown_length         = 0.25,
-    lr_rampup_length           = 0.05,
-    noise_ramp_length          = 0.75,
-    regularize_noise_weight    = 1e5,
-    verbose                    = False,
-    device: torch.device
+        G,
+        target: torch.Tensor,
+        *,
+        num_steps=1000,
+        w_avg_samples=10000,
+        initial_learning_rate=0.1,
+        initial_noise_factor=0.05,
+        lr_rampdown_length=0.25,
+        lr_rampup_length=0.05,
+        noise_ramp_length=0.75,
+        regularize_noise_weight=1e5,
+        verbose=False,
+        device: torch.device
 ):
-    assert target.shape == (G.img_channels, G.img_resolution, G.img_resolution)
+    # Ensure that the target image has the correct shape.
 
     def logprint(*args):
         if verbose:
             print(*args)
 
-    G = copy.deepcopy(G).eval().requires_grad_(False).to(device) # type: ignore
+    # Make a deep copy of the generator and put it in evaluation mode with gradient tracking turned off.
+    G = copy.deepcopy(G).eval().requires_grad_(False).to(device)
 
-    # Compute w stats.
-    logprint(f'Computing W midpoint and stddev using {w_avg_samples} samples...')
+    # Compute the mean and standard deviation of w using random samples.
     z_samples = np.random.RandomState(123).randn(w_avg_samples, G.z_dim)
-    w_samples = G.mapping(torch.from_numpy(z_samples).to(device), None)  # [N, L, C]
-    w_samples = w_samples[:, :1, :].cpu().numpy().astype(np.float32)       # [N, 1, C]
-    w_avg = np.mean(w_samples, axis=0, keepdims=True)      # [1, 1, C]
+    label = torch.zeros([1, G.c_dim], device=device)
+    w_samples = G.mapping(torch.from_numpy(z_samples).to(device), torch.cat([label] * w_avg_samples, dim=0))
+    w_samples = w_samples[:, :1, :].cpu().numpy().astype(np.float32)
+    w_avg = np.mean(w_samples, axis=0, keepdims=True)
     w_std = (np.sum((w_samples - w_avg) ** 2) / w_avg_samples) ** 0.5
 
     # Setup noise inputs.
-    noise_bufs = { name: buf for (name, buf) in G.synthesis.named_buffers() if 'noise_const' in name }
+    noise_bufs = {name: buf for (name, buf) in G.synthesis.named_buffers() if 'noise_const' in name}
 
-    # Load VGG16 feature detector.
+    # Load the VGG16 feature detector.
     url = 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metrics/vgg16.pt'
     with dnnlib.util.open_url(url) as f:
         vgg16 = torch.jit.load(f).eval().to(device)
 
-    # Features for target image.
+    # Compute the features for the target image.
     target_images = target.unsqueeze(0).to(device).to(torch.float32)
     if target_images.shape[2] > 256:
         target_images = F.interpolate(target_images, size=(256, 256), mode='area')
     target_features = vgg16(target_images, resize_images=False, return_lpips=True)
 
-    w_opt = torch.tensor(w_avg, dtype=torch.float32, device=device, requires_grad=True) # pylint: disable=not-callable
+    # Initialize the optimized w.
+    w_opt = torch.tensor(w_avg, dtype=torch.float32, device=device, requires_grad=True)
     w_out = torch.zeros([num_steps] + list(w_opt.shape[1:]), dtype=torch.float32, device=device)
     optimizer = torch.optim.Adam([w_opt] + list(noise_bufs.values()), betas=(0.9, 0.999), lr=initial_learning_rate)
 
@@ -84,87 +89,85 @@ def project(
         lr_ramp = 0.5 - 0.5 * np.cos(lr_ramp * np.pi)
         lr_ramp = lr_ramp * min(1.0, t / lr_rampup_length)
         lr = initial_learning_rate * lr_ramp
+
+        # Set learning rate in optimizer
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr
 
         # Synth images from opt_w.
         w_noise = torch.randn_like(w_opt) * w_noise_scale
         ws = (w_opt + w_noise).repeat([1, G.mapping.num_ws, 1])
+
+        # Generate synthetic images from W vector
         synth_images = G.synthesis(ws, noise_mode='const')
 
         # Downsample image to 256x256 if it's larger than that. VGG was built for 224x224 images.
-        synth_images = (synth_images + 1) * (255/2)
+        synth_images = (synth_images + 1) * (255 / 2)
+        synth_images = synth_images.repeat([1, 3, 1, 1])
         if synth_images.shape[2] > 256:
             synth_images = F.interpolate(synth_images, size=(256, 256), mode='area')
 
-        # Features for synth images.
+        # Compute features of synthetic images using VGG16 network
         synth_features = vgg16(synth_images, resize_images=False, return_lpips=True)
+
+        # Compute distance between target features and synthetic features
         dist = (target_features - synth_features).square().sum()
 
-        # Noise regularization.
+        # Compute noise regularization loss
         reg_loss = 0.0
+
+        # Compute noise regularization loss for each noise buffer.
         for v in noise_bufs.values():
-            noise = v[None,None,:,:] # must be [1,1,H,W] for F.avg_pool2d()
+            noise = v[None, None, :, :]
             while True:
-                reg_loss += (noise*torch.roll(noise, shifts=1, dims=3)).mean()**2
-                reg_loss += (noise*torch.roll(noise, shifts=1, dims=2)).mean()**2
+                reg_loss += (noise * torch.roll(noise, shifts=1, dims=3)).mean() ** 2
+                reg_loss += (noise * torch.roll(noise, shifts=1, dims=2)).mean() ** 2
                 if noise.shape[2] <= 8:
                     break
                 noise = F.avg_pool2d(noise, kernel_size=2)
+
+        # Multiply regularization loss by regularization weight
         loss = dist + reg_loss * regularize_noise_weight
 
         # Step
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
-        logprint(f'step {step+1:>4d}/{num_steps}: dist {dist:<4.2f} loss {float(loss):<5.2f}')
 
         # Save projected W for each optimization step.
         w_out[step] = w_opt.detach()[0]
 
-        # Normalize noise.
-        with torch.no_grad():
-            for buf in noise_bufs.values():
-                buf -= buf.mean()
-                buf *= buf.square().mean().rsqrt()
-
     return w_out.repeat([1, G.mapping.num_ws, 1])
 
-#----------------------------------------------------------------------------
 
 @click.command()
 @click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
 @click.option('--target', 'target_fname', help='Target image file to project to', required=True, metavar='FILE')
-@click.option('--num-steps',              help='Number of optimization steps', type=int, default=1000, show_default=True)
-@click.option('--seed',                   help='Random seed', type=int, default=303, show_default=True)
-@click.option('--save-video',             help='Save an mp4 video of optimization progress', type=bool, default=True, show_default=True)
-@click.option('--outdir',                 help='Where to save the output images', required=True, metavar='DIR')
+@click.option('--num-steps', help='Number of optimization steps', type=int, default=1000, show_default=True)
+@click.option('--seed', help='Random seed', type=int, default=303, show_default=True)
+@click.option('--save-video', help='Save an mp4 video of optimization progress', type=bool, default=True,
+              show_default=True)
+@click.option('--outdir', help='Where to save the output images', required=True, metavar='DIR')
 def run_projection(
-    network_pkl: str,
-    target_fname: str,
-    outdir: str,
-    save_video: bool,
-    seed: int,
-    num_steps: int
+        network_pkl: str,
+        target_fname: str,
+        outdir: str,
+        save_video: bool,
+        seed: int,
+        num_steps: int
 ):
-    """Project given image to the latent space of pretrained network pickle.
+    """Project given image to the latent space of pretrained network pickle."""
 
-    Examples:
-
-    \b
-    python projector.py --outdir=out --target=~/mytargetimg.png \\
-        --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl
-    """
+    # Set the random seed for the numpy and torch libraries.
     np.random.seed(seed)
     torch.manual_seed(seed)
 
-    # Load networks.
-    print('Loading networks from "%s"...' % network_pkl)
+    # Load the pre-trained network pickle.
     device = torch.device('cuda')
     with dnnlib.util.open_url(network_pkl) as fp:
-        G = legacy.load_network_pkl(fp)['G_ema'].requires_grad_(False).to(device) # type: ignore
+        G = legacy.load_network_pkl(fp)['G_ema'].requires_grad_(False).to(device)
 
-    # Load target image.
+    # Load the target image and prepare it for optimization.
     target_pil = PIL.Image.open(target_fname).convert('RGB')
     w, h = target_pil.size
     s = min(w, h)
@@ -172,26 +175,27 @@ def run_projection(
     target_pil = target_pil.resize((G.img_resolution, G.img_resolution), PIL.Image.LANCZOS)
     target_uint8 = np.array(target_pil, dtype=np.uint8)
 
-    # Optimize projection.
+    # Optimize the projection.
     start_time = perf_counter()
     projected_w_steps = project(
         G,
-        target=torch.tensor(target_uint8.transpose([2, 0, 1]), device=device), # pylint: disable=not-callable
+        target=torch.tensor(target_uint8.transpose([2, 0, 1]), device=device),
         num_steps=num_steps,
         device=device,
         verbose=True
     )
-    print (f'Elapsed: {(perf_counter()-start_time):.1f} s')
+    print(f'Elapsed: {(perf_counter() - start_time):.1f} s')
 
     # Render debug output: optional video and projected image and W vector.
     os.makedirs(outdir, exist_ok=True)
     if save_video:
-        video = imageio.get_writer(f'{outdir}/proj.mp4', mode='I', fps=10, codec='libx264', bitrate='16M')
-        print (f'Saving optimization progress video "{outdir}/proj.mp4"')
+        video = imageio.get_writer(f'{outdir}/proj.mp4', mode='I', fps=10, codec='libx264', bitrate='16M',
+                                   ffmpeg_params=["-an"])
         for projected_w in projected_w_steps:
             synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
-            synth_image = (synth_image + 1) * (255/2)
+            synth_image = (synth_image + 1) * (255 / 2)
             synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
+            synth_image = np.repeat(synth_image, 3, axis=2)
             video.append_data(np.concatenate([target_uint8, synth_image], axis=1))
         video.close()
 
@@ -199,14 +203,12 @@ def run_projection(
     target_pil.save(f'{outdir}/target.png')
     projected_w = projected_w_steps[-1]
     synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
-    synth_image = (synth_image + 1) * (255/2)
+    synth_image = (synth_image + 1) * (255 / 2)
     synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
+    synth_image = np.repeat(synth_image, 3, axis=2)
     PIL.Image.fromarray(synth_image, 'RGB').save(f'{outdir}/proj.png')
     np.savez(f'{outdir}/projected_w.npz', w=projected_w.unsqueeze(0).cpu().numpy())
 
-#----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    run_projection() # pylint: disable=no-value-for-parameter
-
-#----------------------------------------------------------------------------
+    run_projection()
